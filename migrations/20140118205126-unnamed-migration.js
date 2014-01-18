3d0428c2773bc30f09f6003219429fb165e0233e
yuya <mumin.nonnon0216@gmail.com>
2014-01-18 21:00:58 +0900

    userテーブルmigration作成

---
+++ migrations/20140118205126-unnamed-migration.js
@@ -0,0 +1,32 @@
+module.exports = {
+  up: function(migration, DataTypes, done) {
+    // add altering commands here, calling 'done' when finished
+      migration.createTabel(
+          "users",
+          {
+            id: {
+              type: DataTypes.INTEGER,
+              autoIncrement: true,
+              primartyKey: true
+            },
+            name: {
+              type: DataTypes.STRING
+            },
+            fp: {
+              type: DataTypes.INTEGER,
+              defaultValue: 0
+            },
+            email: {
+              type: DataTypes.STRING,
+              allowNUll: true
+            }
+          }
+
+      )
+    done()
+  },
+  down: function(migration, DataTypes, done) {
+    // add reverting commands here, calling 'done' when finished
+    done()
+  }
+}
