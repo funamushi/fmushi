02b299436e582001f3c021a92aaf9fddde813d19
f-kubotar <dev@hadashikick.jp>
2015-06-23 23:25:17 +0900

    draw processing

--- src/client/index.js
+++ src/client/index.js
@@ -14,9 +14,20 @@ import 'processing-js';
   let processing = new Processing(canvas, sketch);
 });
 
-function sketch(processing) {
+function sketch(p) {
+  p.size(500, 500);
+
+  let radius = 200;
+  let center = { x: p.width * 0.5, y: p.height * 0.5 };
   // processing.draw = () => {
   //   console.log('draw');
   // };
-  console.log(processing);
+  p.smooth();
+  p.background(230, 230, 230);
+  p.fill(255, 150);
+  p.ellipse(250, 250, radius, radius);
+  p.stroke(130, 0, 0);
+  p.strokeWeight(4);
+  p.line(center.x - 70, center.y - 70, center.x + 70, center.y + 70);
+  p.line(center.x + 70, center.y - 70, center.x - 70, center.y + 70);
 }
