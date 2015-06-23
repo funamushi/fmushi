f99407feffcd25e90bc56acef2894b048340e2e0
f-kubotar <f.kubotar@paperboy.co.jp>
2015-06-23 19:04:48 +0900

    remove pixi toka

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,21 @@
 'use strict';
 
-import pixi from 'pixi';
+import Rx from 'rx';
+import * as processing from 'processing-js';
 
-let documentElement = document.documentElement;
-let {w, h} = 
+console.log(processing);
+
+(function(callback) {
+  if (document.readyState != 'loading'){
+    callback();
+  } else {
+    document.addEventListener('DOMContentLoaded', callback);
+  }
+})(init);
+
+function init() {
+}
+
+function update() {
+}
 
